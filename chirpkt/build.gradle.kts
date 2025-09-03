7d3325775613294d84a826ef08f465fbf311f153
andrey <andrey@moshbear.net>
2025-09-03 19:40:16 -0400

    kotlin: build: move all deps to gradle catalog

--- chirpkt/build.gradle.kts
+++ chirpkt/build.gradle.kts
@@ -3,8 +3,8 @@ plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.ktor)
     alias(libs.plugins.kotlin.plugin.serialization)
-    id("org.flywaydb.flyway") version "11.8.2"
-    id("org.jooq.jooq-codegen-gradle") version "3.20.4"
+    alias(libs.plugins.flyway)
+    alias(libs.plugins.jooq.codegen)
 }
 
 group = "dev.null"
@@ -21,35 +21,18 @@ repositories {
 }
 
 dependencies {
-    api("org.flywaydb:flyway-core:11.8.2")
-    api("org.jooq:jooq:3.20.4")
-    api("org.jooq:jooq-kotlin-coroutines:3.20.4")
-    api("org.xerial:sqlite-jdbc:3.50.1.0")
-    jooqCodegen("org.xerial:sqlite-jdbc:3.50.1.0")
-    runtimeOnly("org.xerial:sqlite-jdbc:3.50.1.0")
-    api("com.zaxxer:HikariCP:6.3.0")
-    implementation("com.fasterxml.uuid:java-uuid-generator:5.1.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.2")
-    implementation("at.favre.lib:bcrypt:0.10.2")
-    implementation("com.sksamuel.hoplite:hoplite-core:2.9.0")
-    implementation("com.sksamuel.hoplite:hoplite-datetime:2.9.0")
-    implementation("com.sksamuel.hoplite:hoplite-hikaricp:2.9.0")
-    implementation("com.sksamuel.hoplite:hoplite-hocon:2.9.0")
-    implementation(libs.ktor.server.core)
-    implementation(libs.ktor.server.resources)
-    implementation(libs.ktor.server.host.common)
-    implementation(libs.ktor.server.status.pages)
-    implementation(libs.ktor.server.content.negotiation)
-    implementation(libs.ktor.serialization.kotlinx.json)
-    implementation(libs.ktor.server.auth)
-    implementation(libs.ktor.server.auth.jwt)
-    implementation(libs.ktor.server.html.builder)
-    implementation(libs.ktor.server.netty)
+    api(libs.bundles.db.deps)
+    jooqCodegen(libs.sqlite)
+    runtimeOnly(libs.sqlite)
+    implementation(libs.uuidgen)
+    implementation(libs.kx.datetime)
+    implementation(libs.bcrypt)
+    implementation(libs.bundles.hoplite)
+    implementation(libs.bundles.ktor.server)
     implementation(libs.logback.classic)
-    implementation(libs.ktor.server.config.yaml)
-    testImplementation(libs.ktor.client.core)
-    testImplementation(libs.ktor.client.content.negotiation)
     testImplementation(libs.ktor.server.test.host)
+    testImplementation(libs.bundles.ktor.client)
+    testImplementation(libs.ktor.client.content.negotiation)
     testImplementation(libs.kotlin.test.junit)
 }
 
